8baa8ceb722f46dca6b0b3aba61485e747b5ce01
mfroembgen <marco.froembgen@gmail.com>
2021-05-07 11:07:35 +0200

    fix: add max resource alarms;

--- main.tf
+++ main.tf
@@ -1,60 +1,112 @@
-module "namespace_label" {
+module "short_label" {
   source  = "applike/label/aws"
   version = "1.1.0"
 
-  delimiter = "/"
+  application = ""
 
   context = module.this.context
 }
 
-resource "aws_cloudwatch_metric_alarm" "cpu-utilization" {
-  count               = var.create ? 1 : 0
-  alarm_name          = "${module.this.family}-${module.this.application}-cpu-utilization-high"
+resource "aws_cloudwatch_metric_alarm" "average-resource-cpu" {
+  count               = module.this.enabled ? 1 : 0
+  alarm_name          = "${module.this.family}-${module.this.application}-average-cpu-high"
   namespace           = "AWS/ECS"
   metric_name         = "CPUUtilization"
   statistic           = "Average"
   treat_missing_data  = var.treat_missing_data
   comparison_operator = "GreaterThanThreshold"
-  threshold           = var.cpu_utilization_threshold
-  period              = var.period
-  evaluation_periods  = var.evaluation_periods
-  datapoints_to_alarm = var.datapoints_to_alarm
+  threshold           = var.average_resource_cpu_threshold
+  period              = var.average_resource_period
+  evaluation_periods  = var.average_resource_evaluation_periods
+  datapoints_to_alarm = var.average_resource_datapoints_to_alarm
 
   dimensions = {
-    ClusterName = "${module.this.project}-${module.this.environment}-${module.this.family}"
+    ClusterName = module.short_label.id
     ServiceName = module.this.application
   }
 
   alarm_actions = [
-    "arn:aws:sns:eu-central-1:164105964448:${module.this.project}-${module.this.environment}-${module.this.family}-alarm"]
-  ok_actions    = [
-    "arn:aws:sns:eu-central-1:164105964448:${module.this.project}-${module.this.environment}-${module.this.family}-alarm"]
+  "arn:aws:sns:eu-central-1:164105964448:${module.short_label.id}-alarm"]
+  ok_actions = [
+  "arn:aws:sns:eu-central-1:164105964448:${module.short_label.id}-alarm"]
 
   tags = module.this.tags
 }
 
-resource "aws_cloudwatch_metric_alarm" "memory-utilization" {
-  count               = var.create ? 1 : 0
-  alarm_name          = "${module.this.family}-${module.this.application}-memory-utilization-high"
+resource "aws_cloudwatch_metric_alarm" "average-resource-memory" {
+  count               = module.this.enabled ? 1 : 0
+  alarm_name          = "${module.this.family}-${module.this.application}-average-memory-high"
   namespace           = "AWS/ECS"
   metric_name         = "MemoryUtilization"
   statistic           = "Average"
   treat_missing_data  = var.treat_missing_data
   comparison_operator = "GreaterThanThreshold"
-  threshold           = var.memory_utilization_threshold
-  period              = var.period
-  evaluation_periods  = var.evaluation_periods
-  datapoints_to_alarm = var.datapoints_to_alarm
+  threshold           = var.average_resource_memory_threshold
+  period              = var.average_resource_period
+  evaluation_periods  = var.average_resource_evaluation_periods
+  datapoints_to_alarm = var.average_resource_datapoints_to_alarm
 
   dimensions = {
-    ClusterName = "${module.this.project}-${module.this.environment}-${module.this.family}"
+    ClusterName = module.short_label.id
     ServiceName = module.this.application
   }
 
   alarm_actions = [
-    "arn:aws:sns:eu-central-1:164105964448:${module.this.project}-${module.this.environment}-${module.this.family}-alarm"]
-  ok_actions    = [
-    "arn:aws:sns:eu-central-1:164105964448:${module.this.project}-${module.this.environment}-${module.this.family}-alarm"]
+  "arn:aws:sns:eu-central-1:164105964448:${module.short_label.id}-alarm"]
+  ok_actions = [
+  "arn:aws:sns:eu-central-1:164105964448:${module.short_label.id}-alarm"]
+
+  tags = module.this.tags
+}
+
+resource "aws_cloudwatch_metric_alarm" "maximum-resource-cpu" {
+  count               = module.this.enabled ? 1 : 0
+  alarm_name          = "${module.this.family}-${module.this.application}-maximum-cpu-high"
+  namespace           = "AWS/ECS"
+  metric_name         = "CPUUtilization"
+  statistic           = "Maximum"
+  treat_missing_data  = var.treat_missing_data
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = var.maximum_resource_cpu_threshold
+  period              = var.maximum_resource_period
+  evaluation_periods  = var.maximum_resource_evaluation_periods
+  datapoints_to_alarm = var.maximum_resource_datapoints_to_alarm
+
+  dimensions = {
+    ClusterName = module.short_label.id
+    ServiceName = module.this.application
+  }
+
+  alarm_actions = [
+  "arn:aws:sns:eu-central-1:164105964448:${module.short_label.id}-info"]
+  ok_actions = [
+  "arn:aws:sns:eu-central-1:164105964448:${module.short_label.id}-info"]
+
+  tags = module.this.tags
+}
+
+resource "aws_cloudwatch_metric_alarm" "maximum-resource-memory" {
+  count               = module.this.enabled ? 1 : 0
+  alarm_name          = "${module.this.family}-${module.this.application}-maximum-memory-high"
+  namespace           = "AWS/ECS"
+  metric_name         = "MemoryUtilization"
+  statistic           = "Maximum"
+  treat_missing_data  = var.treat_missing_data
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = var.maximum_resource_memory_threshold
+  period              = var.maximum_resource_period
+  evaluation_periods  = var.maximum_resource_evaluation_periods
+  datapoints_to_alarm = var.maximum_resource_datapoints_to_alarm
+
+  dimensions = {
+    ClusterName = module.short_label.id
+    ServiceName = module.this.application
+  }
+
+  alarm_actions = [
+  "arn:aws:sns:eu-central-1:164105964448:${module.short_label.id}-info"]
+  ok_actions = [
+  "arn:aws:sns:eu-central-1:164105964448:${module.short_label.id}-info"]
 
   tags = module.this.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,41 +1,65 @@
-variable "create" {
-  type        = bool
-  default     = true
-  description = "Defines if alarm should be created"
-}
-
 variable "treat_missing_data" {
   type        = string
   default     = "breaching"
   description = "What happens to the alarm if no data points are received. One of 'missing', 'ignore', 'breaching' and 'notBreaching'"
 }
 
-variable "datapoints_to_alarm" {
+variable "average_resource_datapoints_to_alarm" {
   type        = number
   default     = 3
   description = "The number of datapoints that must be breaching to trigger the alarm"
 }
 
-variable "evaluation_periods" {
+variable "average_resource_evaluation_periods" {
   type        = number
   default     = 3
   description = "The number of periods over which data is compared to the specified threshold"
 }
 
-variable "period" {
+variable "average_resource_period" {
   type        = number
   default     = 300
   description = "The period in seconds over which the specified statistic is applied"
 }
 
-variable "cpu_utilization_threshold" {
+variable "average_resource_cpu_threshold" {
   type        = number
   default     = 95
   description = "Average of cpu utilization to not be exceeded. Consider keeping some headroom."
 }
 
-variable "memory_utilization_threshold" {
+variable "average_resource_memory_threshold" {
   type        = number
   default     = 95
   description = "Average of memory utilization to not be exceeded. Consider keeping some headroom."
+}
+
+variable "maximum_resource_datapoints_to_alarm" {
+  type        = number
+  default     = 3
+  description = "The number of datapoints that must be breaching to trigger the alarm"
+}
+
+variable "maximum_resource_evaluation_periods" {
+  type        = number
+  default     = 10
+  description = "The number of periods over which data is compared to the specified threshold"
+}
+
+variable "maximum_resource_period" {
+  type        = number
+  default     = 60
+  description = "The period in seconds over which the specified statistic is applied"
+}
+
+variable "maximum_resource_cpu_threshold" {
+  type        = number
+  default     = 150
+  description = "maximum of cpu utilization to not be exceeded. Consider keeping some headroom."
+}
+
+variable "maximum_resource_memory_threshold" {
+  type        = number
+  default     = 150
+  description = "maximum of memory utilization to not be exceeded. Consider keeping some headroom."
 }
